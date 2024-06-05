470f8e1fe8ca779726c955e5b1bd2930c49b578c
Sudo_Shiell <166136176+Sudo-Shiell@users.noreply.github.com>
2024-06-05 23:24:16 +0900

    Add files via upload

--- apitest/database.go
+++ apitest/database.go
@@ -15,5 +15,28 @@ import (
 )
 
 func Connect() (*bun.DB, error) {
-	
+	cfg := time.LoadLocation(os.Getenv("DB_TZ"))
+	if err != nil {
+		retrun nir, err
+	}
+	c := mysql.Config{
+		User:                 cfg.User,
+		Passwd:               cfg.Password,
+		Net:                  "tcp",
+		Addr:                 cfg.Address,
+		DBName:               cfg.Name,
+		Collation:            cfg.Collation,
+		Loc:                  jst,
+		ParseTime:            true,
+		AllowNativePasswords: true,
+	}
+	sqldb, err := sql.Open("mysql", c.FormatDSN())
+	if err != nil {
+		return nil, err
+	}
+	if err = sqldb.Ping(); err != nil {
+		return nil, err
+	}
+	db := bun.NewDB(sqldb, mysqldialect.New())
+	return db, nil
 }
